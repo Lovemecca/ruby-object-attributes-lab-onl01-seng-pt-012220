2579b4d2a5258eb0018b755af5e40c954d06d2a2
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-04 18:39:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,17 @@
-  class Dog 
-  def name = (dogs_name)
-   @name = dogs_name
-  end 
+  class Dog
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
   
   def name
     @name 
-  end 
-  
-  def breed = (breed)
-    @breed  = breed
-  end 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
   
   def breed
-    @breed 
+    @breed
   end 
-end 
-
-
-
+end
